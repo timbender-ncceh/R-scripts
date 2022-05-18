b277ea1240b16adcf9a613eb2f4343a4f2e6e0ab
timbender-ncceh <105810134+timbender-ncceh@users.noreply.github.com>
2022-05-18 15:38:31 -0400

    Update batch_geocode.R

--- batch_geocode.R
+++ batch_geocode.R
@@ -1,31 +1,58 @@
-# Batch Geocode Tool
+# Geocode Tool
 # https://cran.r-project.org/web/packages/censusxy/vignettes/censusxy.html
 
-library(ggplot2)
-library(dplyr)
-library(data.table)
-library(lubridate)
-library(readr)
-library(ggmap)
-# for geocoding and census data
+# Load Necessary Libraries only----
 library(censusxy)
 library(tidycensus)
+library(tigris)
+
+#Sys.getenv("CENSUS_API_KEY") # check census api key
+options(tigris_use_cache = F)
 
 rm(list=ls());cat('\f')
 
-# Vars----
-wd <- "C:/Users/TimBender/Documents/R"
-setwd(wd)
+# functions----
+geoid_tract <- function(gt){
+  if(nchar(gt)!=11){
+    stop("geoid must be 11 chars long")
+  }
+  return(c(state = substr(gt,1,2), county = substr(gt,3,5), tract = substr(gt,6,11)))
+}
 
-# example----
-stl_homicides
-args(cxy_geocode)
-homicide_sf <- cxy_geocode(stl_homicides[1:10,], 
-                           street = "street_address", 
-                           city = "city", 
-                           state = "state", 
-                           zip = "postal_code", class = "sf")
+# batch geocode from neat spreadsheet----
+x <- stl_homicides[1:10,]
+x
 
+cxy_geocode(x, 
+            street = 'street_address', 
+            city = 'city', 
+            state = 'state', 
+            zip = 'postal_code',
+            return = 'locations', # 'locations' or 'geographies'
+            class = 'dataframe',  # 'dataframe' or 'sf' 
+            output = 'full')   # 'simple' or 'full' 
 
-ggplot() + 
-  geom_sf(data = homicide_sf)
+# geocode an un-processed address----
+an.add <- "1060 W Addison St, Chicago, IL 60613"
+
+add1 <- as_tibble(cxy_oneline(address = an.add)) 
+add1
+
+# get geoid for tract----
+geoid.add1 <- tigris::append_geoid(address = data.frame(lat = add1$coordinates.y, 
+                                                        lon = add1$coordinates.x), 
+                                   geoid_type = "tract")$geoid
+
+# get state, county, tract breakout from geoid----
+geoid.tract1 <- geoid_tract(gt = geoid.add1)
+
+# find median hh income for that tract----
+vars.2020_acs5 <- load_variables(2020,"acs5") # prints all the names of the vars for the 5-year american community survey for 2020
+mhh.inc.var <- vars.2020_acs5[vars.2020_acs5$concept == "MEDIAN HOUSEHOLD INCOME IN THE PAST 12 MONTHS (IN 2020 INFLATION-ADJUSTED DOLLARS)",]$name
+
+acs.data <- get_acs(geography = "tract", variables = mhh.inc.var,
+                state = geoid_tract(geoid.add1)["state"], 
+                county = geoid_tract(geoid.add1)["county"], 
+                geometry = F, year = 2020) %>% .[.$GEOID == geoid.add1,]
+
+print(acs.data)
