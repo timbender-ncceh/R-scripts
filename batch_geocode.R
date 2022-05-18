596e90eea00c07865c5fc51824497acbf4493b4d
timbender-ncceh <105810134+timbender-ncceh@users.noreply.github.com>
2022-05-18 12:49:12 -0400

    Update batch_geocode.R

--- batch_geocode.R
+++ batch_geocode.R
@@ -1,1 +1,31 @@
 # Batch Geocode Tool
+# https://cran.r-project.org/web/packages/censusxy/vignettes/censusxy.html
+
+library(ggplot2)
+library(dplyr)
+library(data.table)
+library(lubridate)
+library(readr)
+library(ggmap)
+# for geocoding and census data
+library(censusxy)
+library(tidycensus)
+
+rm(list=ls());cat('\f')
+
+# Vars----
+wd <- "C:/Users/TimBender/Documents/R"
+setwd(wd)
+
+# example----
+stl_homicides
+args(cxy_geocode)
+homicide_sf <- cxy_geocode(stl_homicides[1:10,], 
+                           street = "street_address", 
+                           city = "city", 
+                           state = "state", 
+                           zip = "postal_code", class = "sf")
+
+
+ggplot() + 
+  geom_sf(data = homicide_sf)
