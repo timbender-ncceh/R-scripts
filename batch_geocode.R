d8fe06e390394573412afb34a44b14e9530dee5f
timbender-ncceh <105810134+timbender-ncceh@users.noreply.github.com>
2022-05-19 15:13:05 -0400

    Update batch_geocode.R

--- batch_geocode.R
+++ batch_geocode.R
@@ -6,28 +6,24 @@
 #         spreadsheet
 
 #---- Installing R (https://cran.r-project.org/)
-#       * This tool runs in the R environment, and thus needs to be installed 
-#         on your machine.
-#       * Install the most recent version based on your operating system: 
-#         - Windows:  https://cran.r-project.org/bin/windows/base/
-#         - MacOS:    https://cran.r-project.org/bin/macosx/
-#         - Linux:    'sudo apt install r-base'
+#       * This tool runs in the R environment
+#       * https://cran.r-project.org/
 
 #---- To Use the tool
-#       1) create a new folder, preferably on your C:/ drive (code not tested on 
-#           network drives yet, YMMV)
-#       2) paste a copy of the "geocoding.R" file into your new folder. 
-#       3) place a copy of the spreadsheet "batch_addresses_in.xlsx" in the new folder
+#       1) create a new folder, preferably on your C:/ drive 
+#         i.e. 'C:\Users\<YourName>\Documents\geocode_folder\'
+#       2) open script in web browser: 
+#         https://raw.githubusercontent.com/timbender-ncceh/R-scripts/main/batch_geocode.R
+#       3) <right-click> and save-as "batch_geocode.R" in the new folder 
+#       4) place a copy of the spreadsheet "batch_addresses_in.xlsx" in the new folder
 #           - This spreadsheet can only have 1 sheet of data (i.e. "Sheet1")
 #           - This spreadsheet must have 4 columns labeled exactly as follows: 
-#               1. street_address (always required)
-#               2. city           (2 of 3 required)
-#               3. state          (2 of 3 required)
-#               4. zipcode        (2 of 3 required)
-#       * point-and-click script file
-#       * script will generate a .csv file named "batched_addressed_out.csv" 
-#         with longitude / latitude coordinates as well as a report on the 
-#         quality of the guess of the geocode (i.e. "exact", "close", "poor", etc.)
+#               1. street_address (data required always)
+#               2. city           (1 of 3 can be missing data)
+#               3. state          (1 of 3 can be missing data)
+#               4. zipcode        (1 of 3 can be missing data)
+#       5) double-click "batch_geocode.R" file to run tool
+#       6) script will generate a .csv file named "batched_addressed_out.csv" with results
 
 
 #---- Support
